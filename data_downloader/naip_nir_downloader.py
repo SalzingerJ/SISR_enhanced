4bb5632e120d03eaa56d09c31575ec4bdc14c8fc
Oliver Zendel <oliver.zendel@ait.ac.at>
2025-10-13 18:16:53 +0200

    -downloading only NIR band to save bandwidth and speed-up processing

--- data_downloader/naip_nir_downloader.py
+++ data_downloader/naip_nir_downloader.py
@@ -44,7 +44,7 @@ def webmercator2bbox4326(x,y,z):
 
 # download NAIP data from planetary_computer; roi is based on webmercator coords but result is still in EPSG:4326 (WGS84)
 # padding is necessary to allow valid data also in the future transformed target data space in webmercator
-def download_roi(item0, xyz, trg_filepath, pad_view=[[-0.25,-0.25],[0.25,0.25]], min_dim=1):
+def download_roi(item0, xyz, trg_filepath, pad_view=[[-0.25,-0.25],[0.25,0.25]], min_dim=1, keep_one_band=4):
     signed_item, bbox_4326 = planetary_computer.sign(item0), webmercator2bbox4326(xyz[0],xyz[1],xyz[2])
     signed_url = signed_item.assets.get("image").href
     with rasterio.Env():
@@ -62,29 +62,31 @@ def download_roi(item0, xyz, trg_filepath, pad_view=[[-0.25,-0.25],[0.25,0.25]],
             
             if min([window[i][1]-window[i][0]  for i in range(2)]) < min_dim:
                 return
-            
-            roi_data = src.read(window=window)
+        
+            roi_data = src.read(window=window) if keep_one_band is None else src.read(keep_one_band, window=window)[np.newaxis, :, :]
             meta = src.meta.copy()
+            if not keep_one_band is None:
+                meta.update({"count":1})
             meta.update({
                 "driver": "GTiff",
-                "height": roi_data.shape[1],  # rows
-                "width": roi_data.shape[2],   # cols
+                "height": roi_data.shape[-2],  # rows
+                "width": roi_data.shape[-1],   # cols
                 "transform": src.window_transform(window),
                 "crs": src.crs
             })
             with rasterio.open(trg_filepath, "w", **meta) as dst:
                 dst.write(roi_data)
 
-#load geotiff, transform into target webmercator data; band 3 == NIR
-def convert_roi_nir(geotiff_path, xyz, calc_bandids=[3], dst_wh=(128,128), resampling_method=Resampling.nearest ):
+#load geotiff, transform into target webmercator data; band 4 == NIR for full geotiffs; otherwise 1
+def convert_roi_nir(geotiff_path, xyz, calc_bandids=[1], dst_wh=(128,128), resampling_method=Resampling.nearest ):
     left, bottom, right, top = mercantile.xy_bounds(xyz[0],xyz[1],xyz[2])
     dst_transform = from_bounds(left, bottom, right, top, dst_wh[0], dst_wh[1])
     with rasterio.Env():
         with rasterio.open(geotiff_path) as src:
             dst_array = np.zeros((src.count, dst_wh[1], dst_wh[0]), dtype=np.float32)
             for i in calc_bandids:
-                reproject(  source=rasterio.band(src, i + 1),
-                            destination=dst_array[i],
+                reproject(  source=rasterio.band(src, i),
+                            destination=dst_array[max(0,i-1)],
                             src_transform=src.transform,
                             src_crs=src.crs,
                             dst_transform=dst_transform,
@@ -114,13 +116,14 @@ def add_naip_nir(naip_coll,
         trg_path = '/'.join(op_spl[:-1])+'/nir.png' 
     else: #create parallel structure with same subdirs
         trg_path = f'{trg_rootdir}/{op_spl[-5]}/{op_spl[-4]}/{op_spl[-3]}/{op_spl[-2]}/nir.png'
+
     if skip_existing and os.path.exists(trg_path):
         return
     item0 = naip_coll.get_item(sids_naip[op_spl[-3]])
     time.sleep(sleep_before_sign) #search directly followed by signing op can be too much traffic
     download_roi(item0, xyz, tmp_geotiff_path)
     t0 = time.time()
-    nir_data = convert_roi_nir(tmp_geotiff_path, xyz)[:,:,3]
+    nir_data = convert_roi_nir(tmp_geotiff_path, xyz)
     os.makedirs(os.path.dirname(trg_path),exist_ok=True)
     cv2.imwrite(trg_path, nir_data)
     t1 = time.time()
@@ -128,7 +131,7 @@ def add_naip_nir(naip_coll,
     if time_sleep_open > 0: #conform to webapi; don't spam downloader. No parallel downloads supported
         time.sleep(time_sleep_open)
 
-def main_callable(arg0, tqdm_ver=tqdm, retry_on_err=3, sleep_on_err=5.0):
+def main_callable(arg0, tqdm_ver=tqdm, fail_on_err_retry=4, sleep_on_err=5.0):
     # parser setup
     parser = argparse.ArgumentParser(description="Params")
     parser.add_argument('--src_set', type=str, 
@@ -137,6 +140,7 @@ def main_callable(arg0, tqdm_ver=tqdm, retry_on_err=3, sleep_on_err=5.0):
     parser.add_argument('--trg', type=str, default=None,
                         help="Target NAIP NIR data output root directory; default: put into src root dir")
     args = parser.parse_args(arg0)
+    print("Reading folder "+args.src_set+ " for paths; this process can take a few seconds...")
     if os.path.exists(args.src_set+'/val_set/naip/'): #actual root dir; convert both val_set and train_urban_set
         all_rgb_paths = sorted(glob.glob(args.src_set+'/val_set/naip/**/rgb.png', recursive=True))
         all_rgb_paths += sorted(glob.glob(args.src_set+'/train_urban_set/naip/**/rgb.png', recursive=True))
@@ -144,13 +148,16 @@ def main_callable(arg0, tqdm_ver=tqdm, retry_on_err=3, sleep_on_err=5.0):
         all_rgb_paths = sorted(glob.glob(args.src_set+'/naip/**/rgb.png', recursive=True))
     naip_coll = get_stac_coll()
     for p in tqdm_ver(all_rgb_paths, total=len(all_rgb_paths), desc="Adding NAIP NIR data"):
-        try:
-            for retry_on_err in range(3):
+        for retry_on_err in range(fail_on_err_retry):
+            try:
                 add_naip_nir(naip_coll, p, trg_rootdir=args.trg)
                 break
-        except:
-            time.sleep(sleep_on_err)
-            naip_coll = get_stac_coll()
+            except Exception as ex:
+                if retry_on_err >= fail_on_err_retry-1:
+                    print("Downloader error: ", ex)
+                    return
+                time.sleep(sleep_on_err)
+                naip_coll = get_stac_coll()
 
 if __name__ == "__main__":
     sys.exit(main_callable(sys.argv[1:]))
